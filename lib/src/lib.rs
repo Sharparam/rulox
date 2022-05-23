ef2149b6ecf8a9a19f0c15f2506a7c5737dd4ecf
Adam Hellberg <sharparam@sharparam.com>
2022-05-24 01:59:14 +0200

    Add some documentation for library crate

--- lib/src/lib.rs
+++ lib/src/lib.rs
@@ -1,3 +1,14 @@
+/*!
+This crate provides a library for parsing, compiling, and executing [Lox][] programs.
+
+It is a Rust implementation of the bytecode virtual machine described in the book
+[Crafting Interpreters][crafting-interpreters] by [Robert Nystrom][bob].
+
+[crafting-interpreters]: https://craftinginterpreters.com
+[lox]: https://craftinginterpreters.com/the-lox-language.html
+[bob]: https://stuffwithstuff.com/
+*/
+
 pub mod chunk;
 pub mod value;
 pub mod vm;
